adb26a91a82f8a84598a5d235a6fbe497490ff2c
nabhanRasya <nbhnputra@gmail.com>
2026-06-25 11:10:55 +0700

    chore: configure metro bundler with NativeWind

---
+++ metro.config.js
@@ -0,0 +1,7 @@
+// SDK 56 — gunakan getDefaultConfig dari expo/metro-config
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = withNativeWind(config, { input: "./global.css" });
