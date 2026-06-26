0712ca136df947423980f5717346b0b618486df4
nabhanRasya <nbhnputra@gmail.com>
2026-06-26 09:01:58 +0700

    feat(router): create root layout

---
+++ app/_layout.tsx
@@ -0,0 +1,46 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { queryClient } from "../src/utils/queryClient";
+
+import "../global.css";
+
+export default function RootLayout() {
+  return (
+    <GestureHandlerRootView className="flex-1">
+      <SafeAreaProvider>
+        <QueryClientProvider client={queryClient}>
+          <StatusBar style="light" />
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="(tabs)" />
+            <Stack.Screen
+              name="anime/[id]"
+              options={{ animation: "slide_from_right" }}
+            />
+            <Stack.Screen
+              name="film/[id]"
+              options={{ animation: "slide_from_right" }}
+            />
+            <Stack.Screen
+              name="episode/[id]"
+              options={{
+                animation: "slide_from_right",
+                presentation: "fullScreenModal",
+              }}
+            />
+            <Stack.Screen
+              name="genre/[id]"
+              options={{ animation: "slide_from_right" }}
+            />
+            <Stack.Screen
+              name="catalog/[id]"
+              options={{ animation: "slide_from_right" }}
+            />
+          </Stack>
+        </QueryClientProvider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  );
+}
